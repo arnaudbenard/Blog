768ebc101c54864c6d6216938653c258815e29fe
Arnaud Benard <arnaudbenard13@gmail.com>
2012-07-16 17:48:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogArnaud::Application.config.secret_token = 'e0ffaa58093fe19ee95a0d1d6c571e9a65f62898b50fbc1a3f185342f710c95cbdb91ba72eda2f0e01e8a0c242ae8445dd4facf48e5af92a4a0cf17ea324d85f'
